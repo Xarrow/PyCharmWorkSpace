e84be02e86000db0791a3f3ae2a0010c9f705098
zhangjian <zhangjian12424@gmail.com>
2015-12-16 23:27:53 +0800

    增加注释

--- dropsWooyun2/index.py
+++ dropsWooyun2/index.py
@@ -32,23 +32,36 @@ class WooYunArticle():
 
     def getTagetUrlsFromWebPages(self):
         logging.info("starting getTargetUrlsFromWebPages.")
+        # 记录目标url链接
         f = open(r'targetUrls.txt', 'wb')
-        req = self.helix.session.get(url=self.helix.mainUrl)
+        # 获取乌云知识库首页内容
+        req = self.helix.session.get(url=self.helix.mainUrl, headers=self.helix.headers_pc)
+        # 用beautifulsoup解析
         soup1 = BeautifulSoup(req.text)
+        # start为第一页
         start = 1
+        # 解析获取最后一页
         last = int(soup1.find('a', {'class': 'last'}).get('href').split('/')[-1])
         for i in xrange(start, last, 1):
-            r = self.helix.session.get(url='http://drops.wooyun.org/page/index'.replace('index', str(i)))
+            # 循环获取每一页上内容
+            r = self.helix.session.get(url='http://drops.wooyun.org/page/' + str(i))
             soup2 = BeautifulSoup(r.text)
-            tagNo = self.helix.firstTag.split('/')[-1]
+            # 从配置文件中读取第一条文章的链接
+            firstNo = self.helix.firstTag.split('/')[-1]
+            # 使用beautifulsoup解析出文章的连接
             for i in soup2.find_all("a", {'rel': 'bookmark'}):
-                if tagNo not in i.get('href'):
+                # 判断firstTag 是否在第一条，如果不在就继续抓取，如果在则说明已经抓取过
+                if firstNo !=\
+                        i.get('href'):
+                    # 将目标链接放入 targetPool List当中
                     self.helix.targetPool.append(i.get('href'))
+                    # 将链接写入到文件中，作为记录
                     f.writelines(i.get('title').encode('utf-8') + ':' + i.get('href'))
                     f.write('\r\n')
+                    # 日志记录链接已经写入文件
                     logging.info('%s %s has been saved in targetUrls.txt' % (i.get('title'), i.get('href')))
                 else:
-                    # self.helix.setFistTag(self.helix.targetPool[0])
+                    self.helix.setFistTag(self.helix.targetPool[0])
                     logging.info("抓取完成.")
                     return
         self.helix.setFistTag(self.helix.targetPool[0])
@@ -92,7 +105,7 @@ class WooYunArticle():
                 logging.warn('第%d条 %s : %s 存在问题.' % (i, title, url))
                 logging.info("正在处理异常 喵...........")
                 updatedtitle = str(title).replace('/', '-').encode("utf-8")
-                f = open(r'markdownFile//' + str(time.time()) + '.txt', 'w')
+                f = open(r'markdownFile//' + str(time.time()) + '.md', 'w')
                 f.write(str(html2text2.html2text(content.encode('utf-8'))))
                 f.flush()
                 f.close()
